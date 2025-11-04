1a5c24b14c8bdece96435e4ee88653f48778e41e
Filip-Lukasik <147503758+Filip-Lukasik@users.noreply.github.com>
2025-11-04 03:24:15 +0100

    Add files via upload

---
+++ ADTsinglelist/main.cpp
@@ -0,0 +1,45 @@
+#include <cassert>
+#include "singlelist.h"
+
+int main(){
+	SingleList<int> list = SingleList<int>();
+	assert(list.empty());
+	assert(list.size()==0);
+	list.push_front(1);
+	assert(!list.empty());
+	assert(list.front()==1);
+	assert(list.back()==1);
+	assert(list.size()==1);
+	list.push_front(4);
+	assert(list.front()==4);
+	assert(list.back()==1);
+	assert(list.size()==2);
+	SingleList<int> list2(list);
+	assert(!list2.empty());
+	assert(list2.front()==4);
+	assert(list2.back()==1);
+	assert(list2.size()==2);
+	list.push_back(2);
+	assert(list.back()==2);
+	assert(list.size()==3);
+	assert(list2.back()==1);
+	assert(list2.size()==2);
+	list2 = list;
+	assert(list2.back()==2);
+	assert(list2.size()==3);
+	list.pop_front();
+	assert(list.front()==1);
+	assert(list.size()==2);
+	list.pop_back();
+	assert(list.back()==1);
+	assert(list.size()==1);
+	list2.clear();
+	assert(list2.empty());
+	assert(list2.size()==0);
+	list = list2;
+	assert(list.empty());
+	assert(list.size()==0);
+	SingleList<int> list3(list);
+	assert(list3.empty());
+	assert(list3.size()==0);
+}
